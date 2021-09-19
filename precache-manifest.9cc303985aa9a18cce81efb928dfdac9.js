804125281f0c493e502bebf2d8ad1dce69160fe7
Nenad Misic <nenad.misic997@gmail.com>
2021-09-19 19:39:42 +0200

    Updates

---
+++ precache-manifest.9cc303985aa9a18cce81efb928dfdac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71e13ff818a62c04d887299eafbeb6c",
+    "url": "/affirmation/index.html"
+  },
+  {
+    "revision": "f883d7e4e927977a1b17",
+    "url": "/affirmation/static/js/2.8ae258fd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/affirmation/static/js/2.8ae258fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09440202c810a747265e",
+    "url": "/affirmation/static/js/main.9755bbf4.chunk.js"
+  },
+  {
+    "revision": "7655ac2e23af9e2d2501",
+    "url": "/affirmation/static/js/runtime-main.de50d8d4.js"
+  }
+]);
